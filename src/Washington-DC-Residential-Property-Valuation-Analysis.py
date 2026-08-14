ba693be1284df451fd97caccba8e6d5e28be8035
Alexander Zakrzeski <azakrzeski13@gmail.com>
2026-08-14 16:17:22 -0400

    Add initial files for Washington, DC Residential Property Valuation project

---
+++ src/Washington-DC-Residential-Property-Valuation-Analysis.py
@@ -0,0 +1,3 @@
+# Title: Washington, DC Residential Property Valuation Analysis
+# Author: Alexander Zakrzeski
+# Date: August 14, 2026
---
+++ src/Washington-DC-Residential-Property-Valuation-Application.py
@@ -0,0 +1,3 @@
+# Title: Washington, DC Residential Property Valuation Application 
+# Author: Alexander Zakrzeski
+# Date: August 14, 2026
