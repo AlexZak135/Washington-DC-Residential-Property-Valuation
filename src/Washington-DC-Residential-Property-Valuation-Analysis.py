0f44bb3d4e4ef8b43735eaef61ea2992b321cedd
Alexander Zakrzeski <azakrzeski13@gmail.com>
2026-08-17 21:10:52 -0400

    Update date and enhance data processing in valuation analysis script

--- src/Washington-DC-Residential-Property-Valuation-Analysis.py
+++ src/Washington-DC-Residential-Property-Valuation-Analysis.py
@@ -1,14 +1,18 @@
 # Title: Washington, DC Residential Property Valuation Analysis
 # Author: Alexander Zakrzeski
-# Date: August 14, 2026
+# Date: August 17, 2026
 
 import polars as pl
 
 houses = pl.read_parquet("data/raw/CAMA-Houses.parquet")
 houses = houses.rename(str.lower)
-houses = houses.drop("objectid", "heat", "style", "struct", "grade", "cndtn")
+houses = houses.rename({"intwall_d": "floor_d"})
+houses = houses.drop("objectid", "heat", "style", "struct", "grade", "cndtn", 
+                     "extwall", "roof", "intwall", "gis_last_mod_dttm")
+houses = houses.with_columns(pl.col("ssl").str.replace(r"\s+", " "))
 
-# SSL - String
+
+
 # BATHRM - Int64
 # HF_BATHRM - Int64
 # HEAT_D - String 
@@ -16,6 +20,7 @@ houses = houses.drop("objectid", "heat", "style", "struct", "grade", "cndtn")
 # NUM_UNITS - Int64
 # ROOMS - Int64
 # BEDRM - Int64
+
 # AYB - Int64
 # YR_RMDL - Int64
 # EYB - Int64
@@ -30,15 +35,10 @@ houses = houses.drop("objectid", "heat", "style", "struct", "grade", "cndtn")
 # STRUCT_D - String
 # GRADE_D - String
 # CNDTN_D - String
-
-# EXTWALL - Int64
 # EXTWALL_D - String
-# ROOF - Int64
 # ROOF_D - String
-# INTWALL - Int64
 # INTWALL_D - String
 # KITCHENS - Int64
 # FIREPLACES - Int64
 # USECODE - Int64
 # LANDAREA - Int64
-# GIS_LAST_MOD_DTTM - String
